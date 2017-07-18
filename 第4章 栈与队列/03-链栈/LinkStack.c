e114edef754c32ea02a3cebe912b1ef44a2c3d58
ethancai <ethancai@qq.com>
2017-07-18 14:27:02 +0800

    adjust code format of LinkStack.c

--- 第4章 栈与队列/03-链栈/LinkStack.c
+++ 第4章 栈与队列/03-链栈/LinkStack.c
@@ -135,8 +135,10 @@ int main() {
     Pop(&s, &e);
     printf("弹出的栈顶元素 e=%d\n", e);
     printf("栈空否：%d(1:空 0:否)\n", StackEmpty(s));
+
     GetTop(s, &e);
     printf("栈顶元素 e=%d 栈的长度为%d\n", e, StackLength(s));
+
     ClearStack(&s);
     printf("清空栈后，栈空否：%d(1:空 0:否)\n", StackEmpty(s));
 
