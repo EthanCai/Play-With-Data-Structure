c0454a72cb39d74120392697031e9ef489bad87f
ethancai <ethancai@qq.com>
2017-07-18 15:02:38 +0800

    add collection of execution time

--- 第4章 栈与队列/04-斐波那契函数/Fobinacci.c
+++ 第4章 栈与队列/04-斐波那契函数/Fobinacci.c
@@ -1,35 +1,82 @@
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h"
+#include "time.h"
 
 int Fbi(int i) {    /* 斐波那契的递归函数 */
     if (i < 2)
         return i == 0 ? 0 : 1;
+
     return Fbi(i - 1) + Fbi(i - 2);  /* 这里Fbi就是函数自己，等于在调用自己 */
 }
 
+int Fbi2(int i) {   /* 斐波那契的非递归函数 */
+    int p0 = 1;
+    int p1 = 1;
+
+    if (i == 0) {
+        return p0;
+    }
+
+    if (i == 1) {
+        return p1;
+    }
+
+    int tmp = 0;
+    for (int j = 0; j < i - 1; j++) {
+        tmp = p0 + p1;
+        p0 = p1;
+        p1 = tmp;
+    }
+
+    return p1;
+}
+
 int main() {
-    int i;
-    int a[40];
     printf("迭代显示斐波那契数列：\n");
+
+    clock_t start = clock();
+
+    int a[40];
     a[0] = 0;
     a[1] = 1;
     printf("%d ", a[0]);
     printf("%d ", a[1]);
-    for (i = 2; i < 40; i++) {
+
+    for (int i = 2; i < 40; i++) {
         a[i] = a[i - 1] + a[i - 2];
         printf("%d ", a[i]);
     }
     printf("\n");
 
+    clock_t end = clock();
+    printf("耗时: %f us", (double)(end - start));
     printf("\n");
-    printf("两种不同的方式显示斐波那契数列:\n");
     printf("\n");
+
+
+    printf("迭代函数显示斐波那契数列：\n");
+
+    start = clock();
+    for (int i = 0; i < 40; i++)
+        printf("%d ", Fbi2(i));
+    printf("\n");
+
+    end = clock();
+    printf("耗时: %f us", (double)(end - start));
+    printf("\n");
+    printf("\n");
+
 
     printf("递归显示斐波那契数列：(可以明显地看到递归显示的速度慢，牺牲了性能)\n");
-    for (i = 0; i < 40; i++)
+    start = clock();
+    for (int i = 0; i < 40; i++)
         printf("%d ", Fbi(i));
 
+    printf("\n");
+    end = clock();
+    printf("耗时: %f us", (double)(end - start));
+    printf("\n");
     printf("\n");
 
     system("(pause || read) 2>/dev/null");
