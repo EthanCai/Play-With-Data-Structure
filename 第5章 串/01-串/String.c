158382f2e4bd61d51fb904f6573cfcdbe7627d6a
ethancai <ethancai@qq.com>
2017-07-19 15:17:28 +0800

    add Index method

--- 第5章 串/01-串/String.c
+++ 第5章 串/01-串/String.c
@@ -79,30 +79,80 @@ Status StrCopy(String dest, String src) {
     return OK;
 }
 
-//int StrCompare(String s, String t) {
-//
-//}
-//
+int StrCompare(String s1, String s2) {
+    _Bool isEmpty1 = (s1 == NULL) || *s1 == '\0';
+    _Bool isEmpty2 = (s2 == NULL) || *s2 == '\0';
+
+    if (isEmpty1) {
+        return isEmpty2 ? 0 : -1;
+    } else if (isEmpty2) {
+        return 1;
+    }
+
+    int i = 0;
+    while (1) {
+        _Bool isEnd1 = (s1[i] == '\0');
+        _Bool isEnd2 = (s2[i] == '\0');
+
+        if (isEnd1 && isEnd2) {
+            return 0;
+        } else if (isEnd1 && !isEnd2) {
+            return -1;
+        } else if (!isEnd1 && isEnd2) {
+            return 1;
+        }
+
+        if (s1[i] > s2[i]) {
+            return 1;
+        } else if (s1[i] < s2[i]) {
+            return -1;
+        }
+
+        i++;
+    }
+}
+
+int Index(String s, String t, int pos) {
+    int i = pos;    // 主串s当前字符下标
+    int j = 0;      // 子串t当前字符下标
+
+    int len_s = StrLength(s);
+    int len_t = StrLength(t);
+
+    while(i < len_s && j < len_t) {
+        if (s[i] == t[j]) {
+            i++;
+            j++;
+        } else {
+            i = i - j + 2;      // 退回到上次匹配首位的下一位
+            j = 1;
+        }
+    }
+
+    if (j > len_t - 1) {
+        return i - len_t;
+    } else {
+        return -1;
+    }
+}
+
+
 //void Concat(String t, String s1, String s2) {
 //
 //}
-//
+
 //void SubString(String sub, String s, int pos, int len) {
 //
 //}
-//
-//int Index(String s, String t, int pos) {
-//
-//}
-//
+
 //void Replace(String s, String t, String v) {
 //
 //}
-//
+
 //void StrInsert(String s, int pos, String t) {
 //
 //}
-//
+
 //void StrDelete(String s, int pos, int len) {
 //
 //}
@@ -115,7 +165,8 @@ int main() {
     String s2_1 = (String) malloc(20 * sizeof(char));
     String s2_2 = "hello world!";
     StrCopy(s2_1, s2_2);
-    printf("After copy s2_2 to s2_1, s2_1 is %s\n", s2_1);
+    int len = StrLength(s2_1);
+    printf("After copy s2_2 to s2_1, s2_1 is \"%s\", s2_1's length is %d\n", s2_1, len);
 
     _Bool isEmpty = StringEmpty(s2_1);
     printf(isEmpty ? "s2_1 is empty\n" : "s2_1 is not empty\n");
@@ -124,4 +175,18 @@ int main() {
     printf("After clean s2_1, s2_1 is \"%s\"\n", s2_1);
     _Bool isEmpty2 = StringEmpty("");
     printf(isEmpty2 ? "s2_1 is empty\n" : "s2_1 is not empty\n");
+
+    printf("Compare \"\" with \"\": %d\n", StrCompare("", ""));
+    printf("Compare \"abcde\" with \"abcde\": %d\n", StrCompare("", ""));
+    printf("Compare \"abcde\" with \"abcdef\": %d\n", StrCompare("abcde", "abcdef"));
+    printf("Compare \"abcde\" with \"abcdf\": %d\n", StrCompare("abcde", "abcdf"));
+    printf("Compare \"abcde\" with \"fedwerd\": %d\n", StrCompare("abcde", "abcdf"));
+    printf("Compare \"freiwdj\" with \"ereiwdj\": %d\n", StrCompare("freiwdj", "ereiwdj"));
+    printf("Compare \"ezeiwdj\" with \"ereiwdj\": %d\n", StrCompare("freiwdj", "ereiwdj"));
+    printf("Compare \"ereiwdjquy71276327163a\" with \"ereiwdj\": %d\n", StrCompare("freiwdj", "ereiwdj"));
+
+    printf("\"abc\" indexOf \"eeefffabc12345\": %d\n", Index("eeefffabc12345", "abc", 0));
+    printf("\"abc\" indexOf \"abc12345\": %d\n", Index("abc12345", "abc", 0));
+    printf("\"abc\" indexOf \"eeefffabc\": %d\n", Index("eeefffabc", "abc", 0));
+    printf("\"abc\" indexOf \"eeefffabc\": %d\n", Index("eeefffabc", "abc", 7));
 }
